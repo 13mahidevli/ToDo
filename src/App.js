f9c63512bf0fa6d6dbe5e470dcf7c37c9e581ca1
13mahidevli <13mahidevli@gmail.com>
2024-12-31 23:48:15 +0530

    making changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './App.css'
+import "./App.css";
 export default function App() {
   const [input, setinput] = useState("");
   const [tasks, settasks] = useState([]);
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <div>
-      <h1 className="my-3 mx-2 text-center">todo list</h1>
+      <h1 className="my-4 mx-2 text-center">todo list</h1>
 
       {/* add input and add button */}
       <div className="my-3 mx-2 text-center">
@@ -34,22 +34,39 @@ export default function App() {
           value={input}
           onChange={(e) => setinput(e.target.value)}
         />
-        <button className="mx-2" onClick={addinput}>add</button>
+        <button className="mx-2" onClick={addinput}>
+          add
+        </button>
       </div>
 
       {/* add task */}
-  <ul style={{listStyle:'none'}} className=" text-center">
-    {tasks.map((task) => (
-      <li id={task.id}>
-        {task.text}
-        <button className="mx-2" onClick={(e) => deletes(task.id)}>delete</button>
-      </li>
-    ))}
-  </ul>
+      {tasks.length == 0 ? (
+       <h1 style={{color:'#AE445A'}} className="mx-3 my-2"> "NO PENDING TASK"</h1>
+      ) : (
+        <div className="boxes">
+          <ul style={{ listStyle: "none" }} className=" text-center">
+            {tasks.map((task) => (
+              <li className="box" id={task.id}>
+                {task.text}
+                <button className="mx-2" onClick={(e) => deletes(task.id)}>
+                  delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
 
       {/* add clear button */}
       <div className="text-center">
-        {tasks.length > 0 && <button style={{backgroundColor:'red',color:'white'}} onClick={clearall}>clear all</button>}
+        {tasks.length > 0 && (
+          <button
+            style={{ backgroundColor: "red", color: "white" }}
+            onClick={clearall}
+          >
+            clear all
+          </button>
+        )}
       </div>
     </div>
   );
